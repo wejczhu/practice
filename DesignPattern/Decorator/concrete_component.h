d871e5b4580afe389341af7b1062d811fce73914
Wei jianxing <jx.wei@outlook.com>
2021-10-04 21:57:26 +0800

    DesignPattern: Add Decorator

---
+++ DesignPattern/Decorator/concrete_component.h
@@ -0,0 +1,34 @@
+#ifndef CONCRETE_COMPONENT_H
+#define CONCRETE_COMPONENT_H
+
+#include "component.h"
+
+class HouseBlend : public IBerverage
+{
+public:
+    string Name()
+    {
+        return "HouseBlend";
+    }
+
+    double Cost()
+    {
+        return 30.0;
+    }
+};
+
+class DarkRoast : public IBeverage
+{
+public:
+    string Name()
+    {
+        return "DarkRoast";
+    }
+
+    double Cost()
+    {
+        return 28.5;
+    }
+};
+
+#endif // CONCRETE_COMPONENT_H
