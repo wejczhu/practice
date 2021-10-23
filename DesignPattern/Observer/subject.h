77135e37cf28be96be2627f1071d2f5d81cbc16a
Wei jianxing <jx.wei@outlook.com>
2021-10-23 22:07:28 +0800

    Update code

---
+++ DesignPattern/Observer/subject.h
@@ -0,0 +1,14 @@
+#ifndef SUBJECT_H
+#define SUBJECT_H
+
+class IObserver;
+
+class ISubject
+{
+public:
+    virtual void Attach(IObserver *) = 0;
+    virtual void Detach(IObserver *) = 0;
+    virtual void Notify() = 0
+};
+
+#endif // SUBJECT_H
