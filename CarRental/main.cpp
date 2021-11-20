92172bbeb0134eb3570ecfd22e8423ad43e378ff
Wei jianxing <jx.wei@outlook.com>
2021-11-20 20:59:44 +0800

    Add car rental

---
+++ CarRental/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <windows.h>
+#include <string.h>
+#include <stdio.h>
+#include <time.h>
+#include <cstdlib>
+
+using namespace std;
+
+void customerData();
+int carCount();
+int custCount();
+int rate(int ,int);
+void newCustData();
+void carData();
+void admin();
+void user();
+void menu();
+void fullscreen();
+void art();
+void exitArt();
+void newCarData();
+void delCar();
+void displayCar();
+void existingCust();
