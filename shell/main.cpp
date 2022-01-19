79199bc4ae7d4504c8d8d041ad29aceddd11d9f6
Wei jianxing <jx.wei@outlook.com>
2022-01-19 20:22:16 +0800

    Update code

--- shell/main.cpp
+++ shell/main.cpp
@@ -42,3 +42,24 @@ int lsh_cd(char ** args)
 
     return 1;
 }
+
+int lsh_help(char **args)
+{
+    int i;
+    printf("Stephen Breenan's LSH\n");
+    printf("Type program names and arguments, and hit enter,\n");
+    printf("The following are built in:\n");
+
+    for(i = 0; i < lsh_num_builtins(); i++)
+    {
+        printf(" %s\n", builtin_str[i]);
+    }
+
+    printf("Use the man command for informantion on other programs.\n");
+    return 1;
+}
+
+int lsh_exit(char **args)
+{
+    return 0;
+}
