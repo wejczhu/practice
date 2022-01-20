3558de6d843e93f60512416e7caff9b59beebeab
Wei jianxing <jx.wei@outlook.com>
2022-01-20 20:07:01 +0800

    Update code

--- shell/main.cpp
+++ shell/main.cpp
@@ -63,3 +63,53 @@ int lsh_exit(char **args)
 {
     return 0;
 }
+
+int lsh_launch(char **args)
+{
+    pid_t pid;
+    int status;
+
+    pid = fork();
+    if(pid == 0)
+    {
+        if(execvp(args[0], args) == -1)
+        {
+            perror("lsh");
+        }
+
+        exit(EXIT_FAILURE);
+    }
+    else if(pid < 0)
+    {
+        perror("lsh");
+    }
+    else
+    {
+        do{
+            waitpid(pid, &status, WUNTRACED);
+        }
+        while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+}
+
+int lsh_execute(char **args)
+{
+    int i;
+
+    if(args[0] == NULL)
+    {
+        return 1;
+    }
+
+    for(i = 0; i < lsh_num_builtins(); i++)
+    {
+        if(strcmp(args[0], builtin_str[i]) == 0)
+        {
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return lsh_launch(args);
+}
