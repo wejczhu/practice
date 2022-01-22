732011786385006e8262049611905c6e9b476176
Wei jianxing <jx.wei@outlook.com>
2022-01-22 21:01:09 +0800

    Update code

--- shell/main.cpp
+++ shell/main.cpp
@@ -113,3 +113,69 @@ int lsh_execute(char **args)
 
     return lsh_launch(args);
 }
+
+char *lsh_read_line(void)
+{
+#ifndef LSH_USER_STD_GETLINE
+    char *line = NULL;
+    ssize_t bufsize = 0;
+    if(getline(&line, &bufsize, stdin) == -1)
+    {
+        if(feof(stdin))
+        {
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            perror("lsh: getline\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    return line;
+#else
+#define LSH_RL_BUFSIZE 1024
+    int bufsize = LSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) *bufsize);
+
+    int c;
+    if(!buffer)
+    {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+    } 
+
+    while(1)
+    {
+        c = getchar();
+
+        if(c == EOF)
+        {
+            exit(EXIT_SUCCESS);
+        }
+        else if(c == '\n')
+        {
+            buffer[positon] = '\0';
+            return buffer;
+        }
+        else
+        {
+            buffer[position] = c;
+        }
+
+        position++;
+
+        if(position >= bufsize)
+        {
+            bufsize += LSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if(!buffer)
+            {
+                fprintf(stderr, "lsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+#endif
+}
