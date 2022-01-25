b5097ea42b77a4f34866f0662c522945c9cf1967
Wei jianxing <jx.wei@outlook.com>
2022-01-25 20:25:18 +0800

    Update code

--- shell/main.cpp
+++ shell/main.cpp
@@ -220,3 +220,28 @@ char **lsh_split_line(char *line)
     tokens[position] = NULL;
     return tokens; 
 }
+
+void lsh_loop(void)
+{
+    char *line;
+    char **args;
+    int status;
+
+    do
+    {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
+
+        free(line);
+        free(args);
+    }while (status);
+}
+
+int main(int argc, char** argv)
+{
+    lsh_loop();
+
+    return EXIT_SUCCESS;
+}
