a16e03e9ec1359841815305a4afbea3fb716bc74
Wei jianxing <jx.wei@outlook.com>
2022-02-14 14:10:55 +0800

    Update code

--- cbot/src/cbot.c
+++ cbot/src/cbot.c
@@ -485,7 +485,7 @@ static void cbot_dispatch_msg(struct cbot *bot, struct cbot_message_event event,
     }
 }
 
-void cbot_handler_message(struct cbot *bot, const char *channel,
+void cbot_handle_message(struct cbot *bot, const char *channel,
                           const char *user, const char *message, bool action)
 {
     struct cbot_message_event event;
@@ -509,7 +509,7 @@ void cbot_handler_message(struct cbot *bot, const char *channel,
     cbot_dispatch_msg(bot, event, CBOT_MESSAGG);
 }
 
-void cbot_handler_user_event(struct cbot *bot, const char *channel, 
+void cbot_handle_user_event(struct cbot *bot, const char *channel, 
                              const char *user, enum cbot_event_type type)
 {
     struct cbot_user_event evetn, copy;
@@ -524,6 +524,24 @@ void cbot_handler_user_event(struct cbot *bot, const char *channel,
         copy = event;
         copy.plugin = &hdlr->plugin->p;
         hdlr->handler((struct cbot_event*)&copy, hdlr->user);
+    }
+}
+
+void cbot_handle_nick_event(struct cbot *bot, const char *old_username,
+                            const char *new_username)
+{
+    struct cbot_nick_event event, copy;
+    struct cbot_handler *hdlr;
+    event.bot = bot;
+    event.type = CBOT_NICK;
+    evnet.old_username = old_username;
+    event.new_username = new_username;
+
+    sc_list_for_each_entry(hdlr, &bot->handlers[CBOT_NICK], handler_list, struct cbot_handler)
+    {
+        copy = event;
+        copy.plugin = &hdlr->plugin->p;
+        hdrl->handler((struct cbot_event *)&copy, hdlr->user));
     }
 }
 
