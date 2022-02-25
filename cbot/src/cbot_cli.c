390ad6c6b918d23ea9498327b4f2a8801c7691a0
Wei jianxing <jx.wei@outlook.com>
2022-02-25 21:09:26 +0800

    Update code

--- cbot/src/cbot_cli.c
+++ cbot/src/cbot_cli.c
@@ -10,6 +10,7 @@
 #include "cbot_private.h"
 #include "sc-collections.h"
 
+
 static void cbot_cli_send(const struct cbot *bot, const char *dest
                           const char *msg)
 {
@@ -22,3 +23,18 @@ static void cbot_cli_me(const struct cbot *bot, const char *channel)
     (void)bot;
     printf("[%s]%s: %s\n", dest, bot->name, msg);
 }
+
+static void cbot_cli_op(const struct cbot *bot, const char* channel,
+                        const char* person)
+{
+    (void)bot;
+    printf("[%s!CMD]%s: /op %s\n", channel, bot->name, person);
+
+}                        
+
+static void cbot_cli_join(const struct cbot *bot, const char* channel,
+                          const char *password)
+{
+    (void)bot;
+    printf("[%s~CMD]%s: /join %s\n", channel, bot->name, channel);
+}                          
