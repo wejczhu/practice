4bbabf559b428fc73e22f2d53eb6a905e0b005fe
Wei jianxing <jx.wei@outlook.com>
2022-04-28 20:59:45 +0800

    Update code

---
+++ cbot/src/db.c
@@ -0,0 +1,21 @@
+#include <sqlite3.h>
+#include <stdlib.h>
+
+#include "cbot/cbot.h"
+#include "cbot/db.h"
+#include "cbot_private.h"
+#include "sc-collections.h"
+
+#define CBOTDB_UPSERT_FUNC(name, getter, inserter)
+    int name(struct cbot *bot, char * ARG)
+    {
+        int RV = getter(bot, ARG);
+        if(RV > 0)
+        {
+            return RV;
+        }
+        else
+        {
+            return inserter(bot, ARG);
+        }
+    }
