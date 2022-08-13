9509c5e8bd8c01c3466f49d4a5a34814f1e460bd
Wei jianxing <jx.wei@outlook.com>
2022-08-13 16:52:30 +0800

    Update code

--- cbot/src/log.c
+++ cbot/src/log.c
@@ -17,3 +17,31 @@ void cbot_vlog(int level, const char *format, va_list args)
     }
 }
 
+void cbot_log(int level, const char* format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    cbot_vlog(level, format, args);
+    va_end(args);
+}
+
+void cbot_set_log_level(int level)
+{
+    current_log_level = level;
+}
+
+int cbot_get_log_level(void)
+{
+    return current_log_level;
+}
+
+void cbot_set_log_file(FILE *f)
+{
+    current_log_file = f;
+}
+
+struct levels
+{
+    char *name;
+    int level;
+}
