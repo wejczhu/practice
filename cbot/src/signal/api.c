bf49ec20d135099b1f31b7c4d3003222e9ca7715
Wei jianxing <jx.wei@outlook.com>
2022-09-11 16:47:19 +0800

    Update code

--- cbot/src/signal/api.c
+++ cbot/src/signal/api.c
@@ -31,5 +31,27 @@ void sig_user_free_all(struct sc_list_head *list)
 static struct signal_user *__sig_parse_profile(struct jmsg *jm, size_t ix)
 {
     struct signal_user *user;
-    char 
+    char *nul;
+    size_t namelen;
+
+    user = malloc(sizeof(*user));
+    sc_list_init(&user->list);
+    user->first_name = jmsg_lookup_string_at_len(jm, ix, "name", &namelen);
+    if(user->first_name)
+    {
+        nul = strchr(user->first_name, '\0');
+        if((nul - user->first_name) < namelen)
+        {
+            user->last_name = nul + 1;
+        }
+        else
+        {
+            user->last_name = NULL;
+        }
+    }
+
+    user->number = jmsg_lookup_string_at(jm, ix, "address.number");
+    user->uuid = jmsg_lookup_string_at(jm, ix, "address.uuid");
+
+    return user;
 }
